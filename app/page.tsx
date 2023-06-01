712038a75c2998a8628d0f1f2a388186430d4600
David Gaspar <davidgaspar.dev@gmail.com>
2023-05-31 22:19:12 -0300

    feat(component): add Login stage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
+import { InputSubmit, InputText } from "@/components/Input";
 import ActonLogo from "../assets/acton-logo.png";
 import ActonButton from "../assets/acton-button.png";
 import ActonAvatar from "../assets/acton-avatar.gif";
+import ActonLogin from "../assets/acton-login.png";
+import { UserData } from "@/helpers/types";
 
 export default function MainPage() {
   return (
-    <div className="bg-white w-full h-screen flex flex-col items-center justify-center">
-      <StarterStage onClick={() => {}} />
-    </div>
+    <main className="bg-white w-full h-screen flex flex-col items-center justify-center">
+      {/* <StarterStage onClick={() => {}} /> */}
+      <LoginStage onClick={(user) => console.log("user:", user)} />
+    </main>
   );
 }
 
@@ -41,5 +46,65 @@ function StarterStage(props: StarterStageProps) {
         alt="Acton avatar"
       />
     </>
+  );
+}
+
+type LoginStageProps = {
+  onClick: (user: UserData) => void;
+};
+
+function LoginStage(props: LoginStageProps) {
+  const { onClick } = props;
+
+  const [fullname, setFullname] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [whatsapp, setWhatsapp] = useState<string>();
+  const [sex, setSex] = useState<string>();
+
+  return (
+    <form
+      className="w-full h-full bg-[#7C65B5] flex flex-col items-center justify-center"
+      onSubmit={(event) => {
+        if (!event.defaultPrevented) event.preventDefault();
+
+        if (!fullname || !email || !whatsapp || !sex) {
+          alert("Por favor preenchar todos os campos");
+          return;
+        }
+
+        const user: UserData = {
+          fullname,
+          email,
+          whatsapp,
+          sex,
+        };
+
+        onClick(user);
+      }}
+    >
+      <Image className="mb-10" src={ActonLogin} alt="Acton login logo" width={140} />
+
+      <InputText
+        className="mb-3"
+        placeholder="Nome Completo"
+        text={fullname}
+        onText={setFullname}
+      />
+      <InputText
+        className="mb-3"
+        placeholder="E-mail"
+        text={email}
+        onText={setEmail}
+      />
+      <InputText
+        className="mb-3"
+        placeholder="Whatsapp"
+        text={whatsapp}
+        onText={setWhatsapp}
+      />
+      <InputText className="mb-3" placeholder="Sexo" text={sex} onText={setSex} />
+
+      <InputSubmit className="mt-5 text-[#7C65B5]" name="Avançar" />
+    </form>
   );
 }
