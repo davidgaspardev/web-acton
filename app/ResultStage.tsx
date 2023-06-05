b6a3f0c89ce331517c8f49f4c6564bb899891c2d
David Gaspar <davidgaspar.dev@gmail.com>
2023-06-04 21:59:43 -0300

    fix: set result as dependency on useEffect

--- app/ResultStage.tsx
+++ app/ResultStage.tsx
@@ -17,161 +17,164 @@ export default function ResultStage(props: ResultStageProps) {
   const [result, setResult] = useState<ResultInfoData>();
 
   useEffect(() => {
-    console.log("User:", user);
-    console.log("Result:", result);
-    // for (const questionResult of result) {
-    //   // if (questionResult.id ==)
-    // }
-    const whatDoYouWant = (() => {
-      const reponse = responses.find((response) => response.id === 18)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-    const trainerTime = (() => {
-      const reponse = responses.find((response) => response.id === 19)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-    const level = (() => {
-      switch (trainerTime) {
-        case "Nunca Treinei":
-          return 1;
-        case "6 meses":
-          return 2;
-        case "1 ano":
-          return 4;
-        case "2 anos":
-          return 5;
-        case "3 anos":
-          return 6;
-        default:
-          throw Error("trainerTime expection");
+    if (result === undefined) {
+      const whatDoYouWant = (() => {
+        const reponse = responses.find((response) => response.id === 18)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+      const trainerTime = (() => {
+        const reponse = responses.find((response) => response.id === 19)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+      const level = (() => {
+        switch (trainerTime) {
+          case "Nunca Treinei":
+            return 1;
+          case "6 meses":
+            return 2;
+          case "1 ano":
+            return 4;
+          case "2 anos":
+            return 5;
+          case "3 anos":
+            return 6;
+          default:
+            throw Error("trainerTime expection");
+        }
+      })();
+      const stage = (() => {
+        switch (trainerTime) {
+          case "Nunca Treinei":
+            return 1;
+          case "6 meses":
+            return 6;
+          case "1 ano":
+            return 11;
+          case "2 anos":
+            return 13;
+          case "3 anos":
+            return 16;
+          default:
+            throw Error("trainerTime expection");
+        }
+      })();
+      const methodology = (() => {
+        switch (whatDoYouWant) {
+          case "Perder peso":
+            return level === 1 ? "VIVA LEVE" : "VIVER BEM";
+          case "Qualidade de Vida":
+            return "VIVA LEVE";
+          case "Ganhar Massa Muscular":
+            return level === 1 ? "VIVA LEVE" : "VIDA ATIVA";
+          default:
+            throw Error("trainerTime expection");
+        }
+      })();
+
+      const specialNeedsSelected = new Array<SpecialNeedData>();
+      const questionId3 = (() => {
+        const reponse = responses.find((response) => response.id === 3)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+
+      if (questionId3 === "Sim") {
+        specialNeedsSelected.push(
+          specialNeeds.find((it) => it.name === "Dor Física")!
+        );
       }
-    })();
-    const stage = (() => {
-      switch (trainerTime) {
-        case "Nunca Treinei":
-          return 1;
-        case "6 meses":
-          return 6;
-        case "1 ano":
-          return 11;
-        case "2 anos":
-          return 13;
-        case "3 anos":
-          return 16;
-        default:
-          throw Error("trainerTime expection");
+
+      const questionId4 = (() => {
+        const reponse = responses.find((response) => response.id === 4)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+
+      if (questionId4 === "Sim") {
+        specialNeedsSelected.push(
+          specialNeeds.find(
+            (it) => it.name === "Problemas Cardíacos ou Pressão Alta"
+          )!
+        );
       }
-    })();
-    const methodology = (() => {
-      switch (whatDoYouWant) {
-        case "Perder peso":
-          return level === 1 ? "VIVA LEVE" : "VIVER BEM";
-        case "Qualidade de Vida":
-          return "VIVA LEVE";
-        case "Ganhar Massa Muscular":
-          return level === 1 ? "VIVA LEVE" : "VIDA ATIVA";
-        default:
-          throw Error("trainerTime expection");
+
+      const questionId5 = (() => {
+        const reponse = responses.find((response) => response.id === 5)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+
+      if (questionId5 === "Sim") {
+        specialNeedsSelected.push(
+          specialNeeds.find((it) => it.name === "Diabetes")!
+        );
       }
-    })();
 
-    const specialNeedsSelected = new Array<SpecialNeedData>();
-    const questionId3 = (() => {
-      const reponse = responses.find((response) => response.id === 3)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
+      const questionId8 = (() => {
+        const reponse = responses.find((response) => response.id === 8)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
 
-    if (questionId3 === "Sim") {
-      specialNeedsSelected.push(
-        specialNeeds.find((it) => it.name === "Dor Física")!
-      );
+      if (questionId8 === "Sim") {
+        specialNeedsSelected.push(
+          specialNeeds.find((it) => it.name === "Qualidade de Vida")!
+        );
+      }
+
+      const questionId10 = (() => {
+        const reponse = responses.find((response) => response.id === 10)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+
+      if (questionId10 === "Sim") {
+        console.log(specialNeeds);
+        specialNeedsSelected.push(
+          specialNeeds.find((it) => it.name === "Disposição")!
+        );
+      }
+
+      const questionId11 = (() => {
+        const reponse = responses.find((response) => response.id === 11)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+
+      if (questionId11 === "Sim" && questionId10 === "Não") {
+        specialNeedsSelected.push(
+          specialNeeds.find((it) => it.name === "Disposição")!
+        );
+      }
+
+      const questionId12 = (() => {
+        const reponse = responses.find((response) => response.id === 12)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+
+      if (questionId12 === "Sim") {
+        specialNeedsSelected.push(
+          specialNeeds.find((it) => it.name === "Ansiedade")!
+        );
+      }
+
+      const questionId14 = (() => {
+        const reponse = responses.find((response) => response.id === 14)!;
+        return reponse.answers[reponse.selected![0]];
+      })();
+
+      if (questionId14 === "Sim") {
+        specialNeedsSelected.push(
+          specialNeeds.find((it) => it.name === "Qualidade do Sono")!
+        );
+      }
+
+      setResult({
+        name: user.fullname,
+        methodology,
+        level,
+        stage,
+        specialNeeds: specialNeedsSelected
+          .sort((a, b) => b.priority - a.priority)
+          .slice(0, 3),
+        createdAt: new Date(),
+      });
     }
-
-    const questionId4 = (() => {
-      const reponse = responses.find((response) => response.id === 4)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-
-    if (questionId4 === "Sim") {
-      specialNeedsSelected.push(
-        specialNeeds.find((it) => it.name === "Problemas Cardíacos ou Pressão Alta")!
-      );
-    }
-
-    const questionId5 = (() => {
-      const reponse = responses.find((response) => response.id === 5)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-
-    if (questionId5 === "Sim") {
-      specialNeedsSelected.push(specialNeeds.find((it) => it.name === "Diabetes")!);
-    }
-
-    const questionId8 = (() => {
-      const reponse = responses.find((response) => response.id === 8)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-
-    if (questionId8 === "Sim") {
-      specialNeedsSelected.push(
-        specialNeeds.find((it) => it.name === "Qualidade de Vida")!
-      );
-    }
-
-    const questionId10 = (() => {
-      const reponse = responses.find((response) => response.id === 10)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-
-    if (questionId10 === "Sim") {
-      console.log(specialNeeds);
-      specialNeedsSelected.push(
-        specialNeeds.find((it) => it.name === "Disposição")!
-      );
-    }
-
-    const questionId11 = (() => {
-      const reponse = responses.find((response) => response.id === 11)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-
-    if (questionId11 === "Sim" && questionId10 === "Não") {
-      specialNeedsSelected.push(
-        specialNeeds.find((it) => it.name === "Disposição")!
-      );
-    }
-
-    const questionId12 = (() => {
-      const reponse = responses.find((response) => response.id === 12)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-
-    if (questionId12 === "Sim") {
-      specialNeedsSelected.push(specialNeeds.find((it) => it.name === "Ansiedade")!);
-    }
-
-    const questionId14 = (() => {
-      const reponse = responses.find((response) => response.id === 14)!;
-      return reponse.answers[reponse.selected![0]];
-    })();
-
-    if (questionId14 === "Sim") {
-      specialNeedsSelected.push(
-        specialNeeds.find((it) => it.name === "Qualidade do Sono")!
-      );
-    }
-
-    setResult({
-      name: user.fullname,
-      methodology,
-      level,
-      stage,
-      specialNeeds: specialNeedsSelected
-        .sort((a, b) => b.priority - a.priority)
-        .slice(0, 3),
-      createdAt: new Date(),
-    });
-  }, [user, responses]);
+  }, [user, responses, result]);
   return (
     <div>
       <div>
