531114297a713db7bcba76f2e8ebb7e5a6487aab
David Gaspar <davidgaspar.dev@gmail.com>
2023-08-06 11:45:43 -0300

    feat: create function to generate session code

--- helpers/math.ts
+++ helpers/math.ts
@@ -1,3 +1,7 @@
 export function getPercentageByMeta(current: number, meta: number) {
   return Number((current * 100) / meta);
 }
+
+export function generateSessionCode(): string {
+  return `${Math.random().toString(36).substring(2, 10)}:${Date.now()}`;
+}
