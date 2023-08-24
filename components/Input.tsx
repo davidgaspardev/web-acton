057c87980d56f005feb7145920753d32723fba0e
David Gaspar <davidgaspar.dev@gmail.com>
2023-08-23 22:25:55 -0300

    chore: add password type for Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -4,7 +4,7 @@ type InputProps = {
   label?: string;
   placeholder?: string;
   value?: string;
-  type?: "text" | "number" | "tel" | "email";
+  type?: "text" | "number" | "tel" | "email" | "password";
   className?: string;
   onValue: (value: string) => void;
   pattern?: string;
