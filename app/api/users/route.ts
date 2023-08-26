148e10a094d7883feb695718bd7cc7be58c0e903
David Gaspar <davidgaspar.dev@gmail.com>
2023-08-25 22:53:39 -0300

    chore: check token to use Users API

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,6 @@
+import { DEBUG_MODE, JWT_SECRET_KEY } from "@/helpers/env";
 import prisma from "@/lib/prisma";
+import { decode, verify } from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
 
 /**
@@ -28,31 +30,48 @@ export async function POST(request: Request) {
  * Get all users
  */
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const pageQuery = searchParams.get("page");
+  try {
+    const { searchParams } = new URL(request.url);
+    const pageQuery = searchParams.get("page");
 
-  if (!pageQuery || !Number.isInteger(Number(pageQuery))) {
-    return NextResponse.json({ message: "page query invalid" }, { status: 400 });
+    const authorization = request.headers.get("Authorization");
+    if (!authorization) {
+      return new Response(undefined, { status: 401 });
+    }
+
+    const token = authorization.split(" ")[1];
+
+    const payload = verify(token, JWT_SECRET_KEY);
+
+    if (DEBUG_MODE) {
+      console.log("Token payload:", payload);
+    }
+
+    if (!pageQuery || !Number.isInteger(Number(pageQuery))) {
+      return NextResponse.json({ message: "page query invalid" }, { status: 400 });
+    }
+
+    const page = Number(pageQuery);
+    const pageSize = 100;
+    const usersTotal = await prisma.users.count();
+
+    if ((page - 1) * pageSize >= usersTotal) {
+      return NextResponse.json({ message: "page not exists" }, { status: 400 });
+    }
+
+    const users = await prisma.users.findMany({
+      take: pageSize,
+      skip: (page - 1) * pageSize,
+      include: {
+        results: true,
+      },
+    });
+
+    return NextResponse.json({
+      total: usersTotal,
+      data: users,
+    });
+  } catch (e) {
+    return NextResponse.json(e, { status: 500 });
   }
-
-  const page = Number(pageQuery);
-  const pageSize = 100;
-  const usersTotal = await prisma.users.count();
-
-  if ((page - 1) * pageSize >= usersTotal) {
-    return NextResponse.json({ message: "page not exists" }, { status: 400 });
-  }
-
-  const users = await prisma.users.findMany({
-    take: pageSize,
-    skip: (page - 1) * pageSize,
-    include: {
-      results: true,
-    },
-  });
-
-  return NextResponse.json({
-    total: usersTotal,
-    data: users,
-  });
 }
