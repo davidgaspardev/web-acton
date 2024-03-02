8765a26a4aea88e2ba61340f1ddf52a89df7b300
darckness <joaomarcoslopes48@gmail.com>
2024-03-02 16:12:11 -0300

    Ajust in PopUp

--- components/tmp/PopUp.tsx
+++ components/tmp/PopUp.tsx
@@ -9,7 +9,7 @@ export default function PopUp(props: PopUpProperties): JSX.Element {
     <div className="fixed top-0 end-0 bottom-0 left-0 bg-[#00000075] flex justify-center items-center z-50">
       <div className="max-w-[320px] w-full min-h-[100px] bg-white rounded-lg">
         <div className="p-6 text-center">
-          <h1 className="font-bold mb-4 text-[#67696F] text-lg">Obrigado por realizar seu mepeamento de sintomas conosco!</h1>
+          <h1 className="font-bold mb-4 text-[#67696F] text-lg">Obrigado por realizar seu mapeamento de sintomas conosco!</h1>
           <h4  className="text-[#67696F] mb-4 text-sm">O primeiro passo em busta de mais saúde e diposição já foi dado por você.</h4>
           <h4 className="text-[#67696F] mb-4 text-sm">Em breve nosso time de especialista entrará em contato para mais informações.</h4>
 
