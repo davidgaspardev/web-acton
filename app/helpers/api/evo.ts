eb9d62f8a8ec857bc717e6ecba00b9b37c42f9b4
David Gaspar <davidgaspar.dev@gmail.com>
2024-03-26 07:07:00 -0300

    chore: implementing EVO integration (users)

--- app/helpers/api/evo.ts
+++ app/helpers/api/evo.ts
@@ -1,8 +1,15 @@
+import { EVO_API_BASE_URL, EVO_API_PASSWORD, EVO_API_USERNAME } from "@/helpers/env";
 import { UserData } from "@/helpers/types";
+import { loadBasicAuthHeaderValue } from "../utils/auth";
 
+/**
+ * EVO API client (integration)
+ *
+ * @link https://evo-abc.readme.io/
+ */
 export default class EvoApi {
   private static instance: EvoApi;
-  private constructor() { }
+  private constructor() {}
 
   public static getInstance(): EvoApi {
     if (!this.instance) this.instance = new EvoApi();
@@ -11,10 +18,39 @@ export default class EvoApi {
   }
 
   public createUser = async (userData: UserData) => {
-    try {
+    console.log("I'm here");
 
+    try {
+      const { fullname: name, email, whatsapp: cellphone, gender } = userData;
+
+      const response = await fetch(`${EVO_API_BASE_URL}/v1/prospects`, {
+        method: "POST",
+        headers: {
+          Authorization: loadBasicAuthHeaderValue(
+            EVO_API_USERNAME,
+            EVO_API_PASSWORD
+          ),
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          cellphone,
+          gender,
+          node: "Acton integration",
+        }),
+      });
+
+      if (response.status === 200) {
+        const result = await response.json();
+        console.log("EVO result:", result);
+      } else {
+        throw Error(JSON.stringify(response));
+      }
     } catch (err) {
+      console.error(err);
       throw err;
     }
-  }
+  };
 }
