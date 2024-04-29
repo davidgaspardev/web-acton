afa214e00644e88ab3dc78bbedeb94c4453e90e8
David Gaspar <davidgaspar.dev@gmail.com>
2024-04-29 00:02:44 -0300

    refactor: add PlanType type

--- helpers/types.ts
+++ helpers/types.ts
@@ -152,8 +152,14 @@ export type BrancheInfo = {
   plans: BranchePlan[];
 };
 
+export type PlanType =
+  | "INFINITY START – FIDELIDADE"
+  | "INFINITY START"
+  | "INFINITY MAX"
+  | "ENGENHO DO MEIO CONTRATO DE ADESÃO – FIDELIDADE";
+
 export type BranchePlan = {
-  name: string;
+  name: PlanType;
   link: string;
   value: number;
   benefits: string[];
