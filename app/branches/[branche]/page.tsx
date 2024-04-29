242a2c6f5980b75e74e93f290e935ab49e555a16
David Gaspar <davidgaspar.dev@gmail.com>
2024-04-28 22:02:42 -0300

    chore: building page to branche plans

--- app/branches/[branche]/page.tsx
+++ app/branches/[branche]/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { branches } from "@/helpers/data";
+import { BrancheInfo, BranchePlan, Optional } from "@/helpers/types";
+import { useRef } from "react";
+
 type PageProps = {
   params: {
     branche: string;
@@ -5,13 +11,21 @@ type PageProps = {
 }
 
 export default function Page(props: PageProps) {
-  const { branche } = props.params;
+  const { branche: slug } = props.params;
 
+  const branche = branches.find(branche => branche.slug === slug);
 
+  if(!branche) {
+    return (
+      <div>
+        <h1>Academia não existe</h1>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <h1>{branche}</h1>
+      <h1>{branche.name}</h1>
     </div>
   );
 }
