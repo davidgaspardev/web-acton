99b27683b5b24b24b4ad352a61fffe185d10c880
David Gaspar <davidgaspar.dev@gmail.com>
2024-07-06 08:34:36 -0300

    cleanup: Delete unused page

--- app/branches/components/Banner.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import { Banner } from "./Banner";
-
-describe("Banner", () => {
-  it("should render the Banner component", () => {
-    render(<Banner />);
-  });
-});
