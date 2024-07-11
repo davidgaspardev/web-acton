3ae29178ab574814775b64df68391bf95c2dcba2
David Gaspar <davidgaspar.dev@gmail.com>
2024-07-10 23:36:28 -0300

    feat: Add current location storage functionality

--- helpers/storage.ts
+++ helpers/storage.ts
@@ -1,3 +1,5 @@
+import { Nullable } from "./types";
+
 /**
  * Abstration for local storage from browser
  */
@@ -7,6 +9,7 @@ export default class LocalStorage {
 
   // Address of data in local storage
   private readonly KEY_TOKEN = "TOKEN";
+  private readonly KEY_CURRENT_LOCATION = "CURRENT_LOCATION";
 
   private constructor() {}
 
@@ -28,5 +31,26 @@ export default class LocalStorage {
     const { storage, KEY_TOKEN } = this;
 
     return storage().getItem(KEY_TOKEN);
+  }
+
+  public setLocation(latitude: number, longitude: number) {
+    const { storage, KEY_CURRENT_LOCATION } = this;
+
+    return storage().setItem(
+      KEY_CURRENT_LOCATION,
+      JSON.stringify([latitude, longitude])
+    );
+  }
+
+  public getLoction(): Nullable<[number, number]> {
+    const { storage, KEY_CURRENT_LOCATION } = this;
+
+    const location = storage().getItem(KEY_CURRENT_LOCATION);
+
+    if (location) {
+      return JSON.parse(location);
+    }
+
+    return null;
   }
 }
